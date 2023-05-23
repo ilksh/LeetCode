d63f3c7898b37aac375a92098880a08d540b7583
Sang Hyun Kim <kim3634@purdue.edu>
2023-05-23 23:49:05 +1000

    347. Top K Frequent Elements

---
+++ Medium/0347. Top K Frequent Elements.py
@@ -0,0 +1,19 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        dic  = {} # num, freq
+
+        for num in nums:
+            if num not in dic.keys(): # if the elem is not in key
+                dic[num] = 1    # create a key with a value of 1
+            else: #  num is already exist
+                dic[num] += 1
+        
+        # sort the dictionary by value in descending order
+        sorted_list = sorted(dic.items(), key = lambda x : -x[1])
+        ans = []
+        idx = 0
+        while idx < k : # select most kth elements
+            ans.append(sorted_list[idx][0])
+            idx += 1
+           
+        return ans
