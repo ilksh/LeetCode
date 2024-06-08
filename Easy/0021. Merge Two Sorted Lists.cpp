3d1e2f768b8b018b1016ab3193ee8a1989f62c6e
Sang Hyun Kim <kim3634@purdue.edu>
2024-06-08 16:10:56 +1000

    21. Merge Two Sorted Lists

--- Easy/0021. Merge Two Sorted Lists.cpp
+++ Easy/0021. Merge Two Sorted Lists.cpp
@@ -11,34 +11,30 @@ class Solution {
       ListNode(int x, ListNode *next) : val(x), next(next) {}
     };
 public:
-	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) 
-  {
-		// if list1 happen to be NULL
-		// we will simply return list2.
-		if(l1 == NULL)
-        {
-			return l2;
-		}
-		
-		// if list2 happen to be NULL
-		// we will simply return list1.
-		if(l2 == NULL)
-        {
-			return l1;
-		} 
-		
-		// if value pointend by l1 pointer is less than equal to value pointed by l2 pointer
-		// we wall call recursively l1 -> next and whole l2 list.
-		if(l1 -> val <= l2 -> val)
-        {
-			l1 -> next = mergeTwoLists(l1 -> next, l2);
-			return l1;
-		}
-		// we will call recursive l1 whole list and l2 -> next
-		else
-        {
-			l2 -> next = mergeTwoLists(l1, l2 -> next);
-			return l2;            
-		}
-	}
-};	
+	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* temp = new ListNode();    
+        ListNode* tail = temp;
+
+        while(l1 != nullptr && l2 != nullptr) { 
+            int valOne = l1->val;
+            int valTwo = l2->val;
+
+            if (valOne > valTwo) { // add l2
+                tail->next = l2;
+                l2 = l2->next; // move on to the next node
+            }
+            else { // add l1
+                tail->next = l1;
+                l1 = l1->next; // move on to the next node
+            }
+            tail = tail->next;
+        }
+        // IMPORTANT: current WHILE loop cannot reach to the tail of l1 and l2 
+        if (l1 != nullptr) tail->next = l1;
+        else tail->next = l2;
+
+        ListNode* ans = temp->next;
+        delete temp;
+        return ans;
+  }
+};		
